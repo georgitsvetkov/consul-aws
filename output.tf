8aba673dee0942c98a98da48f54de9600ed1d9f4
georgitsvetkov <jorjetelud@gmail.com>
2022-03-30 10:51:33 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
-# output "instance_ip_addr" {
-#   value = aws_instance.instance.private_ip
-# }
+# source code - https://github.com/brokedba/terraform-examples/blob/master/terraform-provider-aws/launch-instance/outputs.tf
+
+output "SSH_Connection_Server_DC1" {
+     value      = format("ssh connection to instance consul_server_dc1 ==> sudo ssh -i "georgitsvetkov.pem" ubuntu@%s",aws_instance.consul_server_dc1.public_ip)
+}
+
+output "SSH_Connection_Server_DC2" {
+     value      = format("ssh connection to instance consul_server_dc2 ==> sudo ssh -i "georgitsvetkov.pem" ubuntu@%s",aws_instance.consul_server_dc2.public_ip)
+}
